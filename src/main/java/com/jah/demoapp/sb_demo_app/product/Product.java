78c8db6187482c9d79f989bdedddc55a08e9099c
dr-scooby <hinurali@hotmail.com>
2024-08-13 19:46:53 -0700

    updated product class

--- src/main/java/com/jah/demoapp/sb_demo_app/product/Product.java
+++ src/main/java/com/jah/demoapp/sb_demo_app/product/Product.java
@@ -2,4 +2,9 @@ package com.jah.demoapp.sb_demo_app.product;
 
 // product class
 public class Product {
+
+    private Long id; // product id
+    private String name; // product name
+    private double price; // price of product
+
 }
