e1bdc7ea291ce388107e9ae8dc640d61b1972c9d
dr-scooby <hinurali@hotmail.com>
2024-08-13 18:48:25 -0700

    uploading project files

---
+++ src/main/java/com/jah/demoapp/sb_demo_app/SbDemoAppApplication.java
@@ -0,0 +1,15 @@
+package com.jah.demoapp.sb_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemoAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+		SpringApplication.run(SbDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jah/demoapp/sb_demo_app/SbDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jah.demoapp.sb_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
