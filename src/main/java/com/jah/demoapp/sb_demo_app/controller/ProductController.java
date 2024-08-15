aeec7b915a9dfd2d50c663a84ce562d41963ce97
dr-scooby <hinurali@hotmail.com>
2024-08-15 14:21:16 -0700

    added mapping to url hello

--- src/main/java/com/jah/demoapp/sb_demo_app/controller/ProductController.java
+++ src/main/java/com/jah/demoapp/sb_demo_app/controller/ProductController.java
@@ -2,10 +2,16 @@ package com.jah.demoapp.sb_demo_app.controller;
 
 // handle requests from the client
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/producsts")
+@RequestMapping("/products")
 public class ProductController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello jah";
+    }
 }
