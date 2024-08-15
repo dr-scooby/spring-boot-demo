c36307a92d302f6a3ae3290ca2773f2a1e05b8de
dr-scooby <hinurali@hotmail.com>
2024-08-15 14:16:21 -0700

    added requestmapping

--- src/main/java/com/jah/demoapp/sb_demo_app/controller/ProductController.java
+++ src/main/java/com/jah/demoapp/sb_demo_app/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.jah.demoapp.sb_demo_app.controller;
 
 // handle requests from the client
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/producsts")
 public class ProductController {
 }
