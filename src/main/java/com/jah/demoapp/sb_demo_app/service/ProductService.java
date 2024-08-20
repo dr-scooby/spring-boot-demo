b1feda39d2f666dec942a0b7a6dc2aa39b269f54
dr-scooby <hinurali@hotmail.com>
2024-08-19 20:22:40 -0700

    updated ProductService

--- src/main/java/com/jah/demoapp/sb_demo_app/service/ProductService.java
+++ src/main/java/com/jah/demoapp/sb_demo_app/service/ProductService.java
@@ -1,5 +1,19 @@
 package com.jah.demoapp.sb_demo_app.service;
 
 // handle all the logic for Product
+
+import com.jah.demoapp.sb_demo_app.model.Product;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// make it a service, use as injection class to use anywhere
+@Service
 public class ProductService {
+
+
+    public List<Product> getProducts(){
+        return null;
+    }
 }
