be69727a2bb3a3dda205514f3f6ee323d757b231
dr-scooby <hinurali@hotmail.com>
2024-08-26 14:35:10 -0700

    updated code with qty data

--- src/main/java/com/jah/demoapp/sb_demo_app/service/ProductService.java
+++ src/main/java/com/jah/demoapp/sb_demo_app/service/ProductService.java
@@ -21,18 +21,18 @@ public class ProductService {
     private final ProductRepository prodrepos;
 
     // list of products, testing only, move to DB
-    private List<Product> products =  Arrays.asList(
-            new Product(1L, "Samsung", 100, "smart phone"),
+   private List<Product> products =  Arrays.asList(
+            new Product(1L, "Samsung", 100, "smart phone", 1),
 
-            new Product(2L, "Samsung", 100, "S10 smart phone"),
-            new Product(3L, "Samsung", 500, "s20 smart phone"),
-            new Product(4L, "iPhone", 900, "iPhone 10x smart phone"),
-            new Product(5L, "Dell", 550, "laptop"),
-            new Product(6L, "HP", 750, "Laptop"),
-            new Product(7L, "Lenovo", 850, "Laptop"),
-            new Product(8L, "Lenovo", 150, "Laptop x390"),
-            new Product(9L, "Lenovo", 150, "Laptop T15"),
-            new Product(10L, "Lenovo", 150, "Laptop T14")
+            new Product(2L, "Samsung", 100, "S10 smart phone", 1),
+            new Product(3L, "Samsung", 500, "s20 smart phone", 2),
+            new Product(4L, "iPhone", 900, "iPhone 10x smart phone", 2),
+            new Product(5L, "Dell", 550, "laptop", 3),
+            new Product(6L, "HP", 750, "Laptop", 3),
+            new Product(7L, "Lenovo", 850, "Laptop", 3),
+            new Product(8L, "Lenovo", 150, "Laptop x390", 4),
+            new Product(9L, "Lenovo", 150, "Laptop T15", 5),
+            new Product(10L, "Lenovo", 150, "Laptop T14", 6)
     );
 
 
